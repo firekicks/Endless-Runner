f83a202898f99ada41055c334531b8c3c69818fd
Atri Mehta <apmehta@ucsc.edu>
2023-11-08 22:06:56 -0800

    tring to add rocket

--- src/prefab/rocket.js
+++ src/prefab/rocket.js
@@ -2,15 +2,26 @@ class Rocket extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
-        //add object to existing scene displayList and updateList
+        //add object to existing scene
         scene.add.existing(this);
+        //pixels per frame
+       
+    
+    }
 
+    update() {
+        this.x -= 5; 
+        //console.log(this.x);
+        //wrap around from left edge to right edge
         if(this.x <= 0 - this.width) {
             this.x = game.config.width; 
-            //randomize spawn/direction 
-            this.y = Math.floor(Phaser.Math.Between(borderUISize*12 + borderPadding*6, borderUISize*7 + borderPadding*9));
+            this.y = Math.floor(Phaser.Math.Between(borderUISize*12 + borderPadding*6, borderUISize*7 + borderPadding*9));  
         }
+
+       
+
     }
+    //position reset
     reset() {
         //this.x = Math.floor(Math.random()); 
         this.x = game.config.width; 
